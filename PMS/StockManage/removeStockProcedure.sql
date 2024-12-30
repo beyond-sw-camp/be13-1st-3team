5d31f777a54e14380e433551287531c6c7af616a
Sirius147 <lsimma7@naver.com>
2024-12-30 18:18:25 +0900

    Update removeStockProcedure.sql

--- PMS/StockManage/removeStockProcedure.sql
+++ PMS/StockManage/removeStockProcedure.sql
@@ -1,16 +1,27 @@
--- 상품 출고
 DELIMITER $$
 CREATE PROCEDURE RemoveStock(
-	IN r_stock_date DATETIME,
-	IN r_quantity INT,
-	IN r_product_id BIGINT 	
+    IN r_stock_date DATETIME,
+    IN r_quantity INT,
+    IN r_product_id BIGINT
 )
 BEGIN
-	INSERT INTO stock(stock_status, stock_date, r_quantity, product_id)
-	VALUES('출고', r_stock_date, r_quantity ,r_product_id);
-	
-	UPDATE product
-	SET quantity = quantity - r_quantity
-	WHERE product_id = r_product_id;
+    DECLARE current_quantity INT;
+
+    SELECT quantity INTO current_quantity
+    FROM product
+    WHERE product_id = r_product_id;
+
+    IF current_quantity - r_quantity < 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Insufficient stock for the requested operation.';
+    END IF;
+
+
+    INSERT INTO stock(stock_status, stock_date, s_quantity, r_quantity, product_id)
+    VALUES('출고', r_stock_date, 0 , r_quantity, r_product_id);
+
+    UPDATE product
+    SET quantity = quantity - r_quantity
+    WHERE product_id = r_product_id;
 END$$
 DELIMITER ;
